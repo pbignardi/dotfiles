8aa65cad57373bf8ca82992ed990d81ab12ca4c5
Paolo Bignardi <paolo.bigno@gmail.com>
2025-04-04 16:16:54 +0200

    feat: moved dotfiles stowing to script

--- init.sh
+++ init.sh
@@ -406,39 +406,8 @@ if ! [[ -z $name ]] && ! [[ -z $email ]]; then
 fi
 _breakline
 
-# If there are changes in the repo, put out a warning and exit
-_log "Cloning dotfiles repository"
-if [[ $(git -C $DOTFILES status --porcelain) ]]; then
-    # changes
-    _warn "There are pending changes. ${RED}Exiting${NC}"
-    exit 1
-fi
-
-# check if ~/dotfiles exists and is not a git repo
-if [[ -d $DOTFILES ]] && ! git -C $DOTFILES status; then
-    _warn "$DOTFILES exists, but is not a git repo."
-    _info "Moving $DOTFILES to $DOTFILES.old"
-    mv $DOTFILES "$DOTFILES.old"
-fi
-
-# Set SSH as URL remote
-if git -C $DOTFILES status; then
-    cd $DOTFILES
-    git remote set-url origin git@github.com:$USERNAME/dotfiles.git
-    git pull --set-upstream origin
-    git submodule update --init --recursive
-else
-    git clone git@github.com:$USERNAME/dotfiles.git $DOTFILES
-    cd $DOTFILES
-    git submodule update --init --recursive
-fi
-
 # Clone dotfiles repo
-_log "Stowing dotfiles"
-cd $DOTFILES
-# stow required packages
-# TODO: define packages to stow for each system.
-for d in */; do
-    _info "Stowing $d"
-    stow $d
-done
+. pull_dotfiles.sh
+
+# Apply dotfiles
+. stow_dotfiles.sh
