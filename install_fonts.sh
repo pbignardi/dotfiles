17a388966c3cbedcf434f1af8ce7fdafcc3130ed
Paolo Bignardi <paolo.bigno@gmail.com>
2025-04-06 15:19:07 +0200

    feat: install base fonts on top of nerd fonts

---
+++ install_fonts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install fonts manually (no NerdFont -- base font only).
+# Paolo Bignardi - 2025
+
+source common.sh
+
+# determine system
+OS=$(identify_system)
+if [[ -z ${OS:-} ]]; then
+    _error "Unsupported system"
+    exit 1
+fi
+
+# Install Hack font
+if ! $(fc-list | grep "Hack" >/dev/null 2>&1); then
+    case "$OS" in
+        arch) sudo pacman --noconfirm -Syu ttf-hack ;;
+        opensuse*) sudo zypper in -y hack-fonts ;;
+        debian) sudo apt-get -y install fonts-hack-ttf ;;
+        fedora) sudo dnf install -y source-foundry-hack-fonts ;;
+        mac) sudo brew install font-hack ;;
+    esac
+fi
+
+# return to dotfiles
+cd $DOTFILES
