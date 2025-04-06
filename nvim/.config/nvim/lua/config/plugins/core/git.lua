891fd7f6ed8bbf5ff2d40a66b229bc2e28fc22ef
Paolo Bignardi <paolo.bigno@gmail.com>
2025-04-06 23:45:18 +0200

    feat: added neogit to configuration

--- nvim/.config/nvim/lua/config/plugins/core/git.lua
+++ nvim/.config/nvim/lua/config/plugins/core/git.lua
@@ -1,6 +1,15 @@
 return {
-    {
-        "lewis6991/gitsigns.nvim",
-        opts = {},
-    },
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {},
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		config = true,
+	},
 }
