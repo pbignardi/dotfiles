d027b6e428acc3e41b0310f4b34a4e4508a76f9a
Paolo <paolo.bigno@gmail.com>
2025-04-06 10:07:57 +0200

    refact: created function to install nerdfonts

--- install_nerdfonts.sh
+++ install_nerdfonts.sh
@@ -5,58 +5,31 @@
 
 source common.sh
 
-fontlist=$(fc-list)
-if ! $(echo $fontlist | grep "MesloLGM" >/dev/null 2>&1); then
-    _log "Install font: Menlo Nerd Font"
+function font_install() {
+    local specific_font=$2
+    local font_filename=$1
+    if ! $(fc-list | grep $specific_font >/dev/null 2>&1); then
+        _log "Install font: $font_filename Nerd Font"
 
-    ! [[ -d $LOCALSRC ]] && mkdir $LOCALSRC
-    cd $LOCALSRC
-    wget -q --show-progress "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip"
-    unzip -d Meslo Meslo.zip
+        ! [[ -d $LOCALSRC ]] && mkdir $LOCALSRC
+        cd $LOCALSRC
+        wget -q --show-progress "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_filename.zip"
+        unzip -d $font_filename $font_filename.zip
 
-    if [[ ! -d $HOME/.local/share/fonts ]]; then
-        mkdir -p $HOME/.local/share/fonts
+        if [[ ! -d $HOME/.local/share/fonts ]]; then
+            mkdir -p $HOME/.local/share/fonts
+        fi
+        local nf_name="${font_filename}_NF"
+        if [[ -d $HOME/.local/share/fonts/$nf_name ]]; then
+            _warn "Deleting $HOME/.local/share/fonts/$nf_name"
+            rm -rf $HOME/.local/share/fonts/$nf_name
+        fi
+        mv $font_filename $HOME/.local/share/fonts/$nf_name
     fi
-    if [[ -d $HOME/.local/share/fonts/Meslo ]]; then
-        _warn "Deleting $HOME/.local/share/fonts/Meslo"
-        rm -rf $HOME/.local/share/fonts/Meslo
-    fi
-    mv Meslo $HOME/.local/share/fonts/Meslo
-fi
+}
 
-if ! $(echo $fontlist | grep RecMonoLinearNerdFont >/dev/null 2>&1); then
-    _log "Install font: Recurcive Nerd Font"
-
-    ! [[ -d $LOCALSRC ]] && mkdir $LOCALSRC
-    cd $LOCALSRC
-    wget -q --show-progress "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Recursive.zip"
-    unzip -d Recursive Recursive.zip
-
-    if [[ ! -d $HOME/.local/share/fonts ]]; then
-        mkdir -p $HOME/.local/share/fonts
-    fi
-    if [[ -d $HOME/.local/share/fonts/Recursive ]]; then
-        _warn "Deleting $HOME/.local/share/fonts/Recursive"
-        rm -rf $HOME/.local/share/fonts/Recursive
-    fi
-    mv Recursive $HOME/.local/share/fonts/Recursive
-fi
-
-if ! $(echo $fontlist | grep JetBrainsMonoNerdFont >/dev/null 2>&1); then
-    _log "Install font: JetBrainsMono Nerd Font"
-
-    ! [[ -d $LOCALSRC ]] && mkdir $LOCALSRC
-    cd $LOCALSRC
-    wget -q --show-progress "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip"
-    unzip -d JetBrainsMono JetBrainsMono.zip
-
-    if [[ ! -d $HOME/.local/share/fonts ]]; then
-        mkdir -p $HOME/.local/share/fonts
-    fi
-    if [[ -d $HOME/.local/share/fonts/JetBrainsMono ]]; then
-        _warn "Deleting $HOME/.local/share/fonts/JetBrainsMono"
-        rm -rf $HOME/.local/share/fonts/JetBrainsMono
-    fi
-    mv JetBrainsMono $HOME/.local/share/fonts/JetBrainsMono
-fi
-
+font_install "SauceCodePro Nerd Font" "SourceCodePro"
+font_install "MesloLGM" "Meslo"
+font_install "RecMonoLinearNerdFont" "Recursive"
+font_install "JetBrainsMonoNerdFont" "JetBrainsMono"
+font_install "VictorMono Nerd Font" "VictorMono"
