a67c53df43802497e97dac00bd26b5e5d0906c6c
Paolo Bignardi <paolo.bigno@gmail.com>
2025-04-08 18:15:45 +0200

    feat: configured mini plugins to replace other various plugins

--- nvim/.config/nvim/lua/config/plugins/core/mini.lua
+++ nvim/.config/nvim/lua/config/plugins/core/mini.lua
@@ -3,7 +3,60 @@ return {
 	enabled = true,
 	config = function()
 		-- notify
-		require("mini.notify").setup()
+		local notify = require("mini.notify")
+		notify.setup({
+			content = {
+				format = function(notif)
+					local content = ""
+					if notif.level == "INFO" then
+						content = content .. "  Info - "
+					elseif notif.level == "ERROR" then
+						content = content .. "  Error - "
+					elseif notif.level == "WARN" then
+						content = content .. "  Warning - "
+					elseif notif.level == "DEBUG" then
+						content = content .. "  Debug - "
+					else
+						content = ""
+					end
+					return content .. notif.msg .. " "
+				end,
+			},
+			window = {
+				config = {
+					border = "rounded",
+				},
+				max_width_share = 0.4,
+			},
+		})
+		vim.notify = notify.make_notify()
+
+		-- hipatterns
+		local hipatterns = require("mini.hipatterns")
+		hipatterns.setup({
+			highlighters = {
+				fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+				todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+				note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+				hex_color = hipatterns.gen_highlighter.hex_color(),
+			},
+		})
+
+		-- clue (which key)
+		local miniclue = require("mini.clue")
+		miniclue.setup({})
+
+		-- git support
+		require("mini.git").setup()
+
+		-- diff
+		require("mini.diff").setup({
+			view = {
+				style = "sign",
+				signs = { add = "┃", change = "┃", delete = "▁" },
+			},
+		})
 
 		-- ai textobjects
 		require("mini.ai").setup()
