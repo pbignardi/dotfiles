313bc66e85f25f17f7090235edd343620262cd51
Paolo <paolo.bigno@gmail.com>
2025-04-10 21:05:07 +0200

    update: disabled telescope in favor of fzf-lua

--- nvim/.config/nvim/lua/config/plugins/core/fuzzyfinder.lua
+++ nvim/.config/nvim/lua/config/plugins/core/fuzzyfinder.lua
@@ -1,13 +1,21 @@
 return {
+	-- fzf lua
+	{
+		"ibhagwan/fzf-lua",
+		enabled = true,
+		opts = {},
+	},
 	-- Telescope
 	{
 		"nvim-telescope/telescope.nvim",
+		enabled = false,
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	-- Telescope UI Select
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
+		enabled = false,
 		opts = {},
 		config = function()
 			require("telescope").setup({
