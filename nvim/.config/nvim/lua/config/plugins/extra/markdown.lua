2bcb8c2bff18c791a69981b985702817beda0caf
Paolo Bignardi <paolo.bigno@gmail.com>
2025-05-09 12:24:56 +0200

    feat: added markdown support

---
+++ nvim/.config/nvim/lua/config/plugins/extra/markdown.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
+    {
+        "toppair/peek.nvim",
+        event = { "VeryLazy" },
+        build = "deno task --quiet build:fast",
+        config = function()
+            require("peek").setup()
+            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        end,
+    },
+}
