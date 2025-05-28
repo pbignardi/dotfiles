1a90af2e9053a300cd38f9153ec2a6dd02ae5b94
Paolo <paolo.bigno@gmail.com>
2025-05-28 09:07:24 +0200

    feat: added rounded corners to mini.files

--- nvim/.config/nvim/lua/config/plugins/core/files.lua
+++ nvim/.config/nvim/lua/config/plugins/core/files.lua
@@ -2,4 +2,20 @@ return {
 	"echasnovski/mini.files",
 	version = false,
 	opts = {},
+	config = function()
+		require("mini.files").setup()
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "MiniFilesWindowOpen",
+			callback = function(args)
+				local win_id = args.data.win_id
+
+				-- Customize window-local settings
+				vim.wo[win_id].winblend = 50
+				local config = vim.api.nvim_win_get_config(win_id)
+				config.border = "rounded"
+				vim.api.nvim_win_set_config(win_id, config)
+			end,
+		})
+	end,
 }
