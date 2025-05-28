cb4302d1fb3c5388cb7ad689096ace95e7f7847f
Paolo <paolo.bigno@gmail.com>
2025-05-28 09:04:56 +0200

    bugfix: setup nvim-cmp with lazydev.nvim plugin

--- nvim/.config/nvim/lua/config/plugins/lang/lua.lua
+++ nvim/.config/nvim/lua/config/plugins/lang/lua.lua
@@ -1,10 +1,21 @@
 return {
 	{
 		"folke/lazydev.nvim",
-		ft = { "lua" },
-		config = function()
-			require("lazydev.nvim").setup({
-				library = { "nvim-dap-ui" },
+		ft = "lua",
+		opts = {
+			library = {
+				"nvim-dap-ui",
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0,
 			})
 		end,
 	},
