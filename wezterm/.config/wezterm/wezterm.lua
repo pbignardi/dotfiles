1af17bbb7331ccfcafcc8d2eced2b1e6443b3603
Paolo Bignardi <paolo.bigno@gmail.com>
2025-06-02 23:48:42 +0200

    feat: added line number font selector

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -12,20 +12,34 @@ config.window_padding = {
 	bottom = 0,
 }
 
--- fontsize referenced on 96 DPI display
-local ref_fontsize = 13
-local ref_dpi = 96
+local fontsize_setters = {
+	ref_dpi = function(window)
+		-- fontsize referenced on 96 DPI display
+		local ref_fs = 13
+		local ref_dpi = 96
+
+		local active = wezterm.gui.screens().active
+
+		-- compute font size based on the screen dpi
+		window:set_config_overrides({
+			font_size = math.ceil(ref_fs * active.effective_dpi / ref_dpi),
+		})
+	end,
+	line_nr = function(window)
+		-- compute fontsize so that fixed number of lines are visible
+		local target_linenr = 70
+
+		local active = wezterm.gui.screens().active
+		local line_height = active.height / target_linenr
+
+		window:set_config_overrides({
+			font_size = math.ceil(line_height * 72 / active.effective_dpi),
+		})
+	end,
+}
 
 -- update font-size based on dpi settings
-wezterm.on("window-config-reloaded", function(window)
-	local active = wezterm.gui.screens().active
-	local dpi = active.effective_dpi
-	local fontsize = math.ceil(ref_fontsize * ref_dpi / dpi)
-
-	window:set_config_overrides({
-		font_size = fontsize,
-	})
-end)
+wezterm.on("window-config-reloaded", fontsize_setters.line_nr)
 
 -- font configuration
 config.adjust_window_size_when_changing_font_size = false
