284adc803da7ae1f97c39071de7f1fac1bb5b211
Paolo Bignardi <paolo.bigno@gmail.com>
2025-06-17 10:58:54 +0200

    feat: created latex view presets

--- nvim/.config/nvim/lua/config/plugins/lang/latex.lua
+++ nvim/.config/nvim/lua/config/plugins/lang/latex.lua
@@ -1,13 +1,24 @@
 local filetype = { "tex", "latex" }
-return {
-	"lervag/vimtex",
-	ft = filetype,
-	config = function()
+local view_presets = {
+	okular = function()
+		vim.g.vimtex_view_general_viewer = "okular.exe"
+		vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+	end,
+	skim = function()
 		vim.g.vimtex_view_method = "skim"
 		vim.g.vimtex_view_skim_activate = 1
 		vim.g.vimtex_view_skim_no_select = 1
 		vim.g.vimtex_view_skim_reading_bar = 1
 		vim.g.vimtex_view_skim_sync = 1
+	end,
+}
+return {
+	"lervag/vimtex",
+	ft = filetype,
+	config = function()
+		-- view_presets.skim()
+		view_presets.okular()
+
 		vim.g.vimtex_quickfix_open_on_warning = 0
 		vim.g.vimtex_compiler_latexmk = {
 			options = {
