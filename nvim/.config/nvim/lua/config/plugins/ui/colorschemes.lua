7cc6592c516b639810da0f1018c3a9b08c0f000d
Paolo Bignardi <paolo.bigno@gmail.com>
2025-08-16 16:09:23 +0200

    update: changed the spec for colorscheme plugins

--- nvim/.config/nvim/lua/config/plugins/ui/colorschemes.lua
+++ nvim/.config/nvim/lua/config/plugins/ui/colorschemes.lua
@@ -2,7 +2,7 @@ return {
 	"sainnhe/gruvbox-material",
 	"scottmckendry/cyberdream.nvim",
 	"srcery-colors/srcery-vim",
-	"https://github.com/sainnhe/sonokai",
+	"sainnhe/sonokai",
 	"lunacookies/vim-colors-xcode",
 	"dasupradyumna/midnight.nvim",
 	"navarasu/onedark.nvim",
