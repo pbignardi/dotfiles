6b299809302a72b811af7e34fc21a42efa9cf446
Paolo Bignardi <paolo.bigno@gmail.com>
2025-11-20 12:04:31 +0100

    feat: added julia-ls setup to nvim

--- nvim/.config/nvim/lua/config/plugins/editor/lsp.lua
+++ nvim/.config/nvim/lua/config/plugins/editor/lsp.lua
@@ -62,15 +62,46 @@ return {
 			vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
 
 			-- custom lsp setups
-			vim.lsp.config["julials"] = {
-				on_new_config = function(new_config, _)
-					local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
-					if (vim.uv.fs_stat(julia) or {}).type == "file" then
-						vim.notify("Hello!")
-						new_config.cmd[1] = julia
-					end
-				end,
-			}
+			vim.lsp.config("julials", {
+				cmd = {
+					"julia",
+					"--project=" .. "~/.julia/environments/lsp/",
+					"--startup-file=no",
+					"--history-file=no",
+					"-e",
+					[[
+                        using Pkg
+                        Pkg.instantiate()
+                        using LanguageServer
+                    depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
+                    project_path = let
+                        dirname(something(
+                            ## 1. Finds an explicitly set project (JULIA_PROJECT)
+                            Base.load_path_expand((
+                                p = get(ENV, "JULIA_PROJECT", nothing);
+                                    p === nothing ? nothing : isempty(p) ? nothing : p
+                                )),
+                                    ## 2. Look for a Project.toml file in the current working directory,
+                                    ##    or parent directories, with $HOME as an upper boundary
+                                    Base.current_project(),
+                                    ## 3. First entry in the load path
+                                    get(Base.load_path(), 1, nothing),
+                                    ## 4. Fallback to default global environment,
+                                    ##    this is more or less unreachable
+                                Base.load_path_expand("@v#.#"),
+                            ))
+                        end
+                                @info "Running language server" VERSION pwd() project_path depot_path
+                                server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)
+                    server.runlinter = true
+                        run(server)
+                    ]],
+				},
+				filetypes = { "julia" },
+				root_markers = { "Project.toml", "JuliaProject.toml" },
+				settings = {},
+			})
+			vim.lsp.enable("julials")
 
 			-- setup lsp with mason-lspconfig
 			require("mason-lspconfig").setup({
