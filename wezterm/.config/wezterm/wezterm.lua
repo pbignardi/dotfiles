b79896ad5753e1394bf0dcecd95cebde2482b00a
Paolo Bignardi <paolo.bigno@gmail.com>
2026-01-15 17:49:13 +0100

    feat: update config to use wezterm multiplexer

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,12 +1,7 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
+local act = wezterm.action
 
-local is_high_density = function(screen)
-	return screen.effective_dpi >= 144
-end
-
--- disable tab bar
-config.enable_tab_bar = false
+local config = {}
 
 -- disable window padding
 config.window_padding = {
@@ -16,63 +11,88 @@ config.window_padding = {
 	bottom = 0,
 }
 
-local fontsize_setters = {
-	ref_dpi = function(target)
-		if target == nil then
-			target = 16
-		end
-		return function(window)
-			-- fontsize referenced on 72 DPI display
-			local ref_fs = target
+-- multiplexer configuration
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+-- config.tab_bar_at_bottom = true
 
-			local active = wezterm.gui.screens().active
-			local dpi = active.effective_dpi
-
-			if is_high_density(active) then
-				dpi = dpi / 2
-			end
-
-			-- compute font size based on the screen dpi
-			window:set_config_overrides({
-				font_size = math.ceil(ref_fs * 72 / dpi),
-			})
-		end
-	end,
-	line_nr = function(target)
-		if target == nil then
-			target = 70
-		end
-		return function(window)
-			-- compute fontsize so that fixed number of lines are visible
-			local target_linenr = target
-
-			local active = wezterm.gui.screens().active
-			local line_height = active.height / target_linenr
-
-			window:set_config_overrides({
-				font_size = math.ceil(line_height * 72 / active.effective_dpi),
-			})
-		end
-	end,
+-- define unix domain for multiplexing
+config.unix_domains = {
+	{ name = "mux" },
 }
 
--- update font-size based on dpi settings
-wezterm.on("window-config-reloaded", fontsize_setters.ref_dpi(17))
+config.default_domain = "mux"
+
+-- TODO:
+-- define `base` as the default workspace, the one you are on as you
+
+config.leader = {
+	key = "Space",
+	mods = "CTRL",
+	timeout_milliseconds = 2000,
+}
+
+config.keys = {
+	-- tmux-like keybindings
+	{ key = "f", mods = "ALT", action = act.TogglePaneZoomState },
+	{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
+	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = '"', mods = "LEADER|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "%", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "a", mods = "LEADER", action = act.AttachDomain("mux") },
+	{ key = "d", mods = "LEADER", action = act.DetachDomain({ DomainName = "mux" }) },
+	-- navigation stuff
+	{ key = "w", mods = "LEADER", action = act.ShowTabNavigator },
+	{ key = "f", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "WORKSPACES" }) },
+	-- { key = "b", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES|LAUNCH_MENU_ITEMS" }) },
+	{ key = "d", mods = "CTRL", action = act.ShowLauncherArgs({ flags = "DOMAINS" }) },
+	{ key = "d", mods = "LEADER", action = act.DetachDomain("CurrentPaneDomain") },
+}
+for i = 1, 9 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = act.ActivateTab(i - 1),
+	})
+end
+
+wezterm.on("update-status", function(window, pane)
+	-- Make it italic and underlined
+	window:set_right_status(wezterm.format({
+		-- display the current domain
+		{ Foreground = { AnsiColor = "White" } },
+		{ Text = " " .. wezterm.mux.get_domain():name() .. " " },
+		-- display the current workspace
+		{ Background = { AnsiColor = "Green" } },
+		{ Foreground = { AnsiColor = "Black" } },
+		{ Text = " " .. wezterm.mux.get_active_workspace() .. " " },
+	}))
+end)
+
+wezterm.on("format-window-title", function(tab, pane, tabs, panes, config)
+	local zoomed = ""
+	if tab.active_pane_is_zoomed then
+		zoomed = "[Z]"
+	end
+
+	return zoomed .. tab.active_pane.title
+end)
+
+-- INFO: The following is the project directories to search
+local projects = {
+	"C:/Repos",
+}
 
 -- font configuration
 config.adjust_window_size_when_changing_font_size = false
 config.font_size = 13
-config.font = wezterm.font("CaskaydiaMono Nerd Font")
-
--- colorscheme configuration
--- local custom_colorschemes = require("colorschemes")
--- custom_colorschemes.set_custom_colorscheme(config, "Oneminimal_Light")
-config.color_scheme_dirs = { "/home/paolo/.config/wezterm/colors/" }
-config.color_scheme = "NvimGenerated"
-
--- specify wsl on windows
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_domain = "WSL:Debian"
-end
+config.font = wezterm.font("CaskaydiaCove NF")
 
 return config
