0a9a6065b050348d04e8ea845893f9fe4c3a3805
Paolo Bignardi <paolo.bigno@gmail.com>
2026-02-01 16:39:28 +0100

    feat: allow bitwarden system installation for ssh agent

--- unix/.ssh/.setup-ssh.sh
+++ unix/.ssh/.setup-ssh.sh
@@ -2,7 +2,11 @@
 
 # export SSH_AUTH_SOCK variable
 if [[ $(uname -s) == "Linux" ]]; then
-    export SSH_AUTH_SOCK=$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
+    if flatpak list | grep bitwarden &> /dev/null; then
+        export SSH_AUTH_SOCK=$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
+    else
+        export SSH_AUTH_SOCK=$HOME/.bitwarden-ssh-agent.sock
+    fi
 elif [[ $(uname -s) == "Darwin" ]]; then
     export SSH_AUTH_SOCK=$HOME/.bitwarden-ssh-agent.sock
 fi
