082a119bca0aca188760e75ae05093ae7ddf500a
Paolo Bignardi <paolo.bigno@gmail.com>
2026-02-02 09:06:06 +0100

    Revert "feat: allow bitwarden system installation for ssh agent"
    This reverts commit 0a9a6065b050348d04e8ea845893f9fe4c3a3805.

--- unix/.ssh/.setup-ssh.sh
+++ unix/.ssh/.setup-ssh.sh
@@ -2,11 +2,7 @@
 
 # export SSH_AUTH_SOCK variable
 if [[ $(uname -s) == "Linux" ]]; then
-    if flatpak list | grep bitwarden &> /dev/null; then
-        export SSH_AUTH_SOCK=$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
-    else
-        export SSH_AUTH_SOCK=$HOME/.bitwarden-ssh-agent.sock
-    fi
+    export SSH_AUTH_SOCK=$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
 elif [[ $(uname -s) == "Darwin" ]]; then
     export SSH_AUTH_SOCK=$HOME/.bitwarden-ssh-agent.sock
 fi
