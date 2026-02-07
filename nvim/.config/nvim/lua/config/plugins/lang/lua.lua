88d1e05ae0f13bbbef9113328593ca1704d7e661
Paolo Bignardi <paolo@Paolo-PC.localdomain>
2026-02-07 13:29:08 +0100

    feat: switch to blink.cmp for lua

--- nvim/.config/nvim/lua/config/plugins/lang/lua.lua
+++ nvim/.config/nvim/lua/config/plugins/lang/lua.lua
@@ -11,13 +11,18 @@ return {
 		},
 	},
 	{
-		"hrsh7th/nvim-cmp",
-		opts = function(_, opts)
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, {
-				name = "lazydev",
-				group_index = 0,
-			})
-		end,
+		"saghen/blink.cmp",
+		opts = {
+			sources = {
+				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100,
+					},
+				},
+			},
+		},
 	},
 }
