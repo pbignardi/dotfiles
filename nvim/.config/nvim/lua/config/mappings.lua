87cbdcbef473d850ec3831a0bed400fe584271d6
Paolo Bignardi <paolo.bignardi@rse-web.it>
2026-02-12 16:14:33 +0100

    feat: options and base mappings

---
+++ nvim/.config/nvim/lua/config/mappings.lua
@@ -0,0 +1,12 @@
+local map = vim.keymap.set
+
+-- misc
+map("n", "<leader>so", ":source %<CR>", {desc="Source current file"})
+map("v", "<leader>x", ":lua<CR>", { desc = "Run current Lua selection" })
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
+
+-- buffer
+map("n", "j", "v:count == 0? 'gj' : 'j'", { expr = true, silent = true })
+map("n", "k", "v:count == 0? 'gk' : 'k'", { expr = true, silent = true })
+map("n", "<C-u>", "<C-u>zz", { desc = "Half page up" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Half page down" })
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,22 @@
+-- leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- options
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.clipboard = "unnamedplus"
+vim.o.showmatch = true
+vim.o.hlsearch = false
+vim.o.undofile = true
+vim.o.swapfile = false
+vim.o.breakindent = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.termguicolors = true
+vim.o.colorcolumn = "80"
+vim.o.cursorline = true
+vim.o.scrolloff = 8
