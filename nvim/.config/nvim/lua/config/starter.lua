36de6ce2a7edc2cfd7099dbbac843da9d40583a5
Paolo Bignardi <paolo.bignardi@rse-web.it>
2026-03-04 22:35:56 +0100

    feat: added picker to cd into directory

--- nvim/.config/nvim/lua/config/starter.lua
+++ nvim/.config/nvim/lua/config/starter.lua
@@ -1,13 +1,39 @@
 -- STARTUP INTERFACE
 -- setup similar to dashboard-nvim
 
+-- custom mini picker to cd into a directory and create a session there
+local change_dir = function()
+  local matches = MiniPick.get_picker_matches()
+  if not matches.current then
+    return true
+  end
+  -- vim.fn.chdir(matches.current.path)
+  print("Changed directory to: " .. vim.fn.getcwd())
+  return true
+end
+
+local cwd = { "/" }
+
+local open_directory = function()
+  MiniExtra.pickers.explorer({ cwd = cwd }, {
+    mappings = {
+      execute = {
+        char = "<C-CR>",
+        func = change_dir,
+      },
+    },
+  })
+end
+
 local starter = require "mini.starter"
 MiniDeps.now(function()
   starter.setup {
+    header = "",
     footer = "",
     items = {
       starter.sections.sessions(),
-      starter.sections.pick(),
+      { name = "Directory", section = "Open", action = open_directory },
+      { name = "File", section = "Open", action = function() end },
       starter.sections.builtin_actions(),
     },
     content_hooks = {
